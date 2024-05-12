f173d0107a68b39ab16324ca4674b97f8008cc05
ehrbhein <theofficial.irvingil@gmail.com>
2024-05-12 09:03:06 +0800

    ✨ feat: add register user modal

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  RegisterForm: FormGroup = new FormGroup({
+    username: new FormControl('', Validators.required),
+    role: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
 
+  ngOnInit(): void {
+    this.initializeForm;
+  }
+
+  initializeForm() {
+    this.RegisterForm.setValue({
+      username: 'yourUserName',
+      role: 'Developer',
+      password: '',
+    });
+  }
+
+  onSubmit() {
+    console.log(this.RegisterForm);
+  }
 }
