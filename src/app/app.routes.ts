ca86067e91f7d7e7ef7e43eeb1edba8c4c966029
ehrbhein <theofficial.irvingil@gmail.com>
2024-05-12 18:14:54 +0800

    ✨ feat: start tasks page impl

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login/login.component';
+import { LoginComponent } from './login/login.component';
+import { TasksComponent } from './tasks/tasks.component';
 
-export const routes: Routes = [{ path: '', component: LoginComponent }];
+export const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'tasks', component: TasksComponent },
+];
